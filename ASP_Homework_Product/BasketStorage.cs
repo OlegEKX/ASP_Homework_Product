cbeed19da1c117847262185341c8f72797435090
OlegEKX <olegjarl@gmail.com>
2024-03-21 16:24:55 +0300

    Осуществил добавление в корзину предмета, кнопка которого была нажата

---
+++ ASP_Homework_Product/BasketStorage.cs
@@ -0,0 +1,16 @@
+﻿using ASP_Homework_Product.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ASP_Homework_Product
+{
+    public class BasketStorage
+    {
+        static List<Product> productsInBasket = new List<Product>();
+
+        public void AddToBasket()
+        {
+
+        }
+    }
+}
--- ASP_Homework_Product/Controllers/BasketController.cs
+++ ASP_Homework_Product/Controllers/BasketController.cs
@@ -1,15 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP_Homework_Product.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace ASP_Homework_Product.Controllers
 {
     public class BasketController : Controller
     {
+        private readonly BasketStorage productsInBasket;
         private readonly ProductStorage productStorage;
 
         public BasketController()
         {
+            productsInBasket = new BasketStorage();
             productStorage = new ProductStorage();
         }
+
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        public IActionResult Index(int id)
+        {
+            var products = productStorage.GetProducts();
+            foreach (var item in products)
+            {
+                if (item.Id == id)
+                {
+                    productsInBasket.AddToBasket(item);
+                    return View(item);
+                }
+            }
+
+
+            return View(products);
+        }
+
+
+
+        /*private readonly ProductStorage productStorage;
+        public BasketController()
+        {
+            productStorage = new ProductStorage();
+        }*/
 
         /// <summary>
         /// завершить добавление товара в корзину,
@@ -19,12 +52,17 @@ namespace ASP_Homework_Product.Controllers
         /// </summary>
         /// <returns></returns>
         //
-        public IActionResult Index()
+        /*public IActionResult Index()
         {
             var products = productStorage.GetProducts();
-            return View(products);
-        }
 
+            return View(products[1]);
+        }*/
+
+        /*public IActionResult Index(Product product)
+        {
+            return View(product);
+        }*/
         
     }
 }
