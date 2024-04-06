a8b1274cb008a0b21b5812f1353cbc2e9fa11963
OlegEKX <olegjarl@gmail.com>
2024-04-06 21:59:33 +0300

    добавил форму для оформления заказа при заполнении данных и нажатии на кнопку оформления заказа, появляется форма об успешной покупке, а корзина пользователя очищается

---
+++ ASP_Homework_Product/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ASP_Homework_Product.Models;
+using Microsoft.VisualBasic;
+
+namespace ASP_Homework_Product.Controllers
+{
+	public class OrderController : Controller
+	{
+
+		private readonly IBasketStorage basketStorage;
+		private readonly IConstants constants;
+
+		public OrderController(IBasketStorage basketStorage, IConstants constants)
+		{
+			this.basketStorage = basketStorage;
+			this.constants = constants;
+		}
+
+		public IActionResult Index()
+		{
+			var basket = basketStorage.TryGetByUserId(constants.UserId);
+			return View(basket);
+		}
+
+		public IActionResult MakeOrder()
+		{
+			basketStorage.Clear(constants.UserId);
+			return View("~/Views/Order/Success.cshtml");
+		}
+	}
+}
