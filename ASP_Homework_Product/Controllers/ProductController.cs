6ab1d71281f8154cb8c31cbad888c856d8633dc1
OlegEKX <olegjarl@gmail.com>
2024-05-12 16:38:20 +0300

    Добавил Атрибуты для класса productEdit (он используется как для редактирования, так и для добавления нового товара) Добавил тег-хелперы к обоим представлениям (для редактирования и добавления) Добавил валидацию на стороне сервера для этих двух функций

--- ASP_Homework_Product/Controllers/ProductController.cs
+++ ASP_Homework_Product/Controllers/ProductController.cs
@@ -31,34 +31,49 @@ namespace ASP_Homework_Product.Controllers
 		public IActionResult Edit(int id)
 		{
             var product = productStorage.TryGetById(id);
+            ProductEdit editProduct = new ProductEdit(product.Name, product.Cost, product.Description, product.ImageURL, product.Id);
 
-			return View(product);
+			return View(editProduct);
 		}
 
 		[HttpPost]
 		public IActionResult Edit(ProductEdit productEdit, int id)
 		{
-			var product = productStorage.TryGetById(id);
+            /*if (productEdit.Name == productEdit.Description)
+            {
+                ModelState.AddModelError("", "Название и описание не должны совпадать");
+            }*/
 
-			product.Name = productEdit.Name;
-			product.Cost = productEdit.Cost;
-			product.Description = productEdit.Description;
-			product.ImageURL = productEdit.ImageURL;
+            if (ModelState.IsValid)
+            {
+				var product = productStorage.TryGetById(id);
 
-			return RedirectToAction("Products", "Administrator");
+				product.Name = productEdit.Name;
+				product.Cost = productEdit.Cost;
+				product.Description = productEdit.Description;
+				product.ImageURL = productEdit.ImageURL;
+
+				return RedirectToAction("Products", "Administrator");
+			}
+            return RedirectToAction("Edit");
 		}
 
         public IActionResult Add()
         {
+            ProductEdit productEdit = new ProductEdit();
             return View();
         }
 
         [HttpPost]
         public IActionResult Add(ProductEdit newProduct)
         {
-            List<Product> products = productStorage.GetProducts();
-            products.Add(new Product(newProduct.Name, newProduct.Cost, newProduct.Description, newProduct.ImageURL));
-            return RedirectToAction("Products","Administrator");
+            if (ModelState.IsValid)
+            {
+				List<Product> products = productStorage.GetProducts();
+				products.Add(new Product(newProduct.Name, newProduct.Cost, newProduct.Description, newProduct.ImageURL));
+				return RedirectToAction("Products", "Administrator");
+			}
+            return RedirectToAction("Add");
         }
 	}
 }
--- ASP_Homework_Product/Models/ProductEdit.cs
+++ ASP_Homework_Product/Models/ProductEdit.cs
@@ -1,10 +1,39 @@
-﻿namespace ASP_Homework_Product.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Homework_Product.Models
 {
 	public class ProductEdit
 	{
+		[Required(ErrorMessage = "Заполните название товара")]
+		[StringLength(30, MinimumLength = 4, ErrorMessage = "Длина названия должна быть от 4 до 30 символов")]
 		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Заполните поле для цены")]
+		[Range(100, 1000000, ErrorMessage = "Цена должжна иметь диапазон от 100 до 1 000 000")]
 		public int Cost { get; set; }
+
+		[Required(ErrorMessage = "Пару слов о товаре")]
+		[StringLength(300, MinimumLength = 20, ErrorMessage = "Длина описания должна быть от 20 до 300 символов")]
 		public string Description { get; set; }
-		public string ImageURL { get; set; } 
+
+		[Required(ErrorMessage = "Обязательно для вставки картинки в карту продукта")]
+		
+		public string ImageURL { get; set; }
+
+		public int Id { get; set; }
+
+		public ProductEdit()
+		{
+
+		}
+
+		public ProductEdit(string name, int cost, string description, string imageURL, int id)
+		{
+			Name = name;
+			Cost = cost;
+			Description = description;
+			ImageURL = imageURL;
+			Id = id;
+		}
 	}
 }
